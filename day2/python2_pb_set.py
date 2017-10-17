bee0ccc3c3c783c8ca853cb1d450f6f321a109ed
admin <admin@pfb08.cshl.edu>
2017-10-17 17:18:20 -0400

    add python2_pb_set ex1-0

--- day2/python2_pb_set.py
+++ day2/python2_pb_set.py
@@ -2,5 +2,9 @@
 import sys
 
 my_dna = ' GTACCTTGATTTCGTATTCTGAGAGGCTGCTGCTTAGCGGTAGCCCCTTGGTTTCCGTGGCAACGGAAAA'
-print('my_dna') 
-print('TCT' in my_dna)
+print(my_dna) 
+
+if 'TCT' in my_dna:
+    print('found TCT in my_dna')
+else:
+    print(' did not find TCT in my_dna')
