82841e822b4aff9876ec12f26c21aaf9e4f2f73c
admin <admin@pfb08.cshl.edu>
2017-10-17 11:26:27 -0400

    pythin first commit, exercise 1 code

---
+++ day2/python1_pb_set.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys 
+
+# print nyc
+#print("Hello New York")
+
+# store and print name
+#my_name = "Guillaume"
+#print(my_name)
+
+# store and print several things
+#my_fav_col = "green"
+#my_fav_act = "coding"
+#my_fav_ani = "monkeys"
+#print("My name is " + my_name +", my favorite color is " + my_fav_col + ",  i like " + my_fav_act + " and " + my_fav_ani)
+
+# use sys.argv and launch like:  ./python1_pb_set.py a b c d 
+a = sys.argv[1]
+b = sys.argv[2]
+c = sys.argv[3]
+d = sys.argv[4]
+
+print("My name is " + a  +", my favorite color is " + b + ",  i like " + c + " and " + d)
+
+ 
