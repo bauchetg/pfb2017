337798f13b244eaf74b3f7c38fc99cf5cf53f296
admin <admin@pfb08.cshl.edu>
2017-10-22 18:04:11 -0400

    add day5-6 pb set

--- day6/NGS1_pb_set.py
+++ day6/NGS1_pb_set.py
@@ -9,25 +9,23 @@ fastq_read = open ("pfb.fastq", "r")
 line_num_tot = ''
 line_cat = ''
 seq_mn = ''
+line_tot = 0 
 
 for line_num, line in enumerate(fastq_read):
-	lines_all += line
+	line_tot += 1
 	line_num_tot = line_num % 4
-	#print(line_num_tot)
 	if line_num_tot == 1:
-		# strip \n
 		line = line.rstrip()
 		line_cat += line
-		#seq_lent.append(len(line))
 #get lenght of seq
 seq_len = len(line_cat)
-#print(seq_len)
-# use list comprehnsion to turn str into list of ints (or  alt sol.: #seq_ls_nb = map(ord(seq_ls)))
+#list comprehension: turn str in list of int (or  alt sol.: #seq_ls_nb = map(ord(seq_ls)))
 seq_ls_nb = [ord(nuc) for nuc in line_cat]
 seq_mn = sum(seq_ls_nb)/seq_len
-#print('{:.1f}'.format(seq_mn)) 
 # get std dev using numpy
 seq_stdev = np.std(seq_ls_nb)
 
-print('sequence lenght is', seq_len, ' bp,  mean quality score is', '{:.1f}'.format(seq_mn), 'standard deviation is ', {:.1f}'.format(seq_stdev))
-
+print("total seq length:", seq_len)
+print("average read length is", int(line_tot/4))
+print("Average quality score is:", '{:.1f}'.format(seq_mn))
+print("Standard deviation is:", '{:.1f}'.format(seq_stdev))
